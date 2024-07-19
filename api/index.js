b55d82f20348ebabe8afb3a555d953d3947bef05
p-k-pavan <pkpavan2003@email.com>
2024-07-19 17:28:12 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log("Sever is running on 3000 port")
-})
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.log("Error to connect DB");
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Sever is running on 3000 port");
+});
