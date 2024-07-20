56c5c9548cb30e53a05817ddce4d9666e9c998b2
p-k-pavan <pkpavan2003@email.com>
2024-07-20 11:00:47 +0530

    Create sign in API route

--- api/index.js
+++ api/index.js
@@ -7,12 +7,18 @@ import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB)
+  .connect(process.env.MONGODB, {  serverSelectionTimeoutMS: 30000 })
   .then(() => {
     console.log("Connected to DB");
   })
   .catch((error) => {
-    console.log("Error to connect DB");
+    console.error("Error connecting to DB:", error.message);
+
+    if (error.message.includes('timed out')) {
+      console.error('Connection attempt timed out. Please check your database server and network settings.');
+    } else {
+      console.error('An unexpected error occurred:', error);
+    }
   });
 
 const app = express();
