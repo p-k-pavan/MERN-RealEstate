abddb80bed44cae47bc0b553060b388a16658398
p-k-pavan <pkpavan2003@email.com>
2024-07-22 16:20:03 +0530

    Update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
-  return <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/about' element={<About/>} />
-    <Route path='/sign-in' element={<Signin/>} />
-    <Route path='/sign-up' element={<SignUp/>} />
-    <Route path='/profile' element={<Profile/>} />
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,9 +34,15 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="sign-in">
+          <Link to="/profile">
             <li className=" text-slate-700 hover:underline cursor-pointer hover:font-bold">
-              Sign in
+              {currentUser ? (
+                <img className="w-7 rounded-full object-cover" src={currentUser.photo} alt="profile" />
+              ) : (
+                <li className="text-slate-700 hover:underline ">
+                  Sign In
+                </li>
+              )}
             </li>
           </Link>
         </ul>
