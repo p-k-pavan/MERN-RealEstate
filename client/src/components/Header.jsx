bce7945419f689fa0ecac6d28d1fe70549f3c0d6
p-k-pavan <pkpavan2003@email.com>
2024-07-30 13:37:49 +0530

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm,setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+  };
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+
+  },[location.search])
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -15,13 +34,17 @@ export default function Header() {
             <span className="text-slate-700">Estate</span>
           </Link>
         </h1>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center cursor-pointer">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center cursor-pointer">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button>
           <FaSearch className="text-slate-600"></FaSearch>
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
