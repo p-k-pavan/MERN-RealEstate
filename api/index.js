b1b80c69f9d1ec0f62d040a44dcbc957a1a547ea
p-k-pavan <pkpavan2003@email.com>
2024-07-31 19:44:07 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.router.js"
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 
@@ -23,6 +24,8 @@ mongoose
     }
   });
 
+const __dirname  = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -35,6 +38,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
