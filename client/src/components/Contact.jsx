7cbfca748602ca798a252b3ed795933c6f8a4bcb
p-k-pavan <pkpavan2003@email.com>
2024-08-01 12:54:32 +0530

    contact page fixing error

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState("");
-  const [error,setError] = (null);
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
 
   useEffect(() => {
     const fetchLandlord = async () => {
@@ -12,44 +15,36 @@ export default function Contact({ listing }) {
         const data = await res.json();
         setLandlord(data);
       } catch (error) {
-        setError(error.message);
+        console.log(error);
       }
     };
     fetchLandlord();
   }, [listing.userRef]);
-
-  const onChange = (e) => {
-    setMessage(e.target.value);
-  };
-
   return (
     <>
       {landlord && (
         <div className="flex flex-col gap-2">
           <p>
-            Contact{" "}
-            <span className="font-semibold">
-              {landlord.username.toUpperCase()}
-            </span>{" "}
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
             for{" "}
-            <span className="font-semibold"> {listing.name.toLowerCase()}</span>
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
           </p>
           <textarea
             name="message"
-            id=""
+            id="message"
             rows="2"
             value={message}
-            placeholder="Enter your message here.."
-            className="w-full border p-3 rounded-lg"
             onChange={onChange}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded-lg"
           ></textarea>
-          <a
-            href={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-            className="bg-slate-700 text-white p-3 uppercase text-center rounded-lg hover:opacity-95 mt-4"
+
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
           >
             Send Message
-          </a>
-          {error && <p className="text-red-700">{error}</p>}
+          </Link>
         </div>
       )}
     </>
